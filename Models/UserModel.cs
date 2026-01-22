abb68d31cc742a4f01a7943f0d9de67c173c9451
gtapiero <giovanny.tapiero@pi.edu.co>
2026-01-22 12:44:40 -0500

    commit - UserModel / .

---
+++ Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace oracle_api_demo.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
