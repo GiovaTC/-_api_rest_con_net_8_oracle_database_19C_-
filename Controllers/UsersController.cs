aa53b90e597eb09b3a02c665d1220590cdf4b469
gtapiero <giovanny.tapiero@pi.edu.co>
2026-01-22 13:14:53 -0500

    commit - UsersController / .

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using oracle_api_demo.Data;
+using oracle_api_demo.Models;   
+
+namespace oracle_api_demo.Controllers
+{
+    [ApiController]
+    [Route("api/[users]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly OracleRepository _repository;
+
+        public UsersController(OracleRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] UserModel user)
+        {
+            _repository.InsertUser(user);
+            return Ok(new { message = "usuario registrado correctamente!" });
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _repository.GetUsers();
+            return Ok(users);
+        }
+    }
+}   
